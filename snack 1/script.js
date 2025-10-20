e6eba4d33225f44b4db16f8b0229e9f1bf084d9a
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-20 09:54:02 +0200

    crate logic to define lightest bike of the array

--- snack 1/script.js
+++ snack 1/script.js
@@ -5,23 +5,41 @@ Stampare a schermo la bici con peso minore.*/
 
 const bicycles = [
     {
-        name: '',
+        name: 'Numero-1',
         weight: 10
     },
     {
-        name: '',
+        name: 'Numero-2',
         weight: 15
     },
     {
-        name: '',
+        name: 'Numero-3',
         weight: 12
     },
     {
-        name: '',
+        name: 'Numero-4',
         weight: 9
     },
     {
-        name: '',
+        name: 'Numero-5',
         weight: 11
     }
 ]
+
+//creato l'array, inizio con il ciclo for per la ricerca dela bici con peso minore
+
+//creo la mia variabile da stampare e prendo come bici più leggera la prima così da poter iniziare il confronto
+let bike = bicycles[0]
+
+
+for(let i=0; i<bicycles.length; i++) {
+    const thisObject = bicycles[i] //richiamo l'oggetto i-esimo
+
+    //se thisObject ha peso minore di bike, bike (ovvero la bici più leggera) divente proprio thisObject
+    if(thisObject.weight < bike.weight) {
+        bike = thisObject
+    }
+}
+console.log(bike); //stampa della bici più leggera
+
+
