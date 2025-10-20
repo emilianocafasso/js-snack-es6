893a32bab327e702aaee827189599b5352ce39ce
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-20 09:46:05 +0200

    create bicycles array

---
+++ snack 1/script.js
@@ -0,0 +1,27 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+const bicycles = [
+    {
+        name: '',
+        weight: 10
+    },
+    {
+        name: '',
+        weight: 15
+    },
+    {
+        name: '',
+        weight: 12
+    },
+    {
+        name: '',
+        weight: 9
+    },
+    {
+        name: '',
+        weight: 11
+    }
+]
